6e71c036cca3e365b967a1a9e752801947ff5fc3
João de Deus <jdjunior2@stefanini.com>
2022-05-11 14:26:42 -0300

    add springdoc-openapi and swagger configurations

--- src/main/java/com/joaodcpjunior/pizzas/controllers/PizzaController.java
+++ src/main/java/com/joaodcpjunior/pizzas/controllers/PizzaController.java
@@ -2,7 +2,12 @@ package com.joaodcpjunior.pizzas.controllers;
 
 import com.joaodcpjunior.pizzas.dtos.PizzaDTO;
 import com.joaodcpjunior.pizzas.entities.Pizza;
+import com.joaodcpjunior.pizzas.exceptions.StandardError;
 import com.joaodcpjunior.pizzas.services.PizzaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,8 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/pizzas")
@@ -22,27 +25,60 @@ public class PizzaController {
     private PizzaService pizzaService;
 
     @PostMapping
+    @Operation(
+            summary = "Register a new Pizza",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Created"),
+                    @ApiResponse(responseCode = "404", description = "Object not Found")
+            }
+    )
     public ResponseEntity<Void> registerPizza(@RequestBody PizzaDTO pizzaDto) {
         Pizza pizza = pizzaService.fromDto(pizzaDto);
         pizzaService.registerPizza(pizza);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(pizza.getId()).toUri();
-        return  ResponseEntity.created(uri).build();
+        return ResponseEntity.created(uri).build();
     }
 
     @GetMapping
-    public ResponseEntity<?> findAll(Pageable pageable){
+    @Operation(
+            summary = "Find all Pizzas",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ok"),
+            }
+    )
+    public ResponseEntity<?> findAll(@RequestParam(required = false) Pageable pageable) {
         Page<Pizza> pizzaList = pizzaService.findAll(pageable);
         Page<PizzaDTO> pizzaDTOList = pizzaList.map(pizza -> new PizzaDTO(pizza));
         return ResponseEntity.ok().body(pizzaDTOList);
     }
 
     @GetMapping(value = "/{id}")
+    @Operation(
+            summary = "Find a Pizza by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ok", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PizzaDTO.class)
+                    )),
+                    @ApiResponse(responseCode = "404", description = "Object not Found", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StandardError.class)
+                    ))
+            }
+    )
     public ResponseEntity<PizzaDTO> findById(@PathVariable Integer id) {
         Pizza pizzaObj = pizzaService.findById(id);
         return ResponseEntity.ok().body(new PizzaDTO(pizzaObj));
     }
 
     @PutMapping(value = "/{id}")
+    @Operation(
+            summary = "Update a Pizza by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ok"),
+                    @ApiResponse(responseCode = "404", description = "Object not Found")
+            }
+    )
     public ResponseEntity<Void> update(@RequestBody PizzaDTO pizzaDTO, @PathVariable Integer id) {
         Pizza pizzaObj = pizzaService.fromDto(pizzaDTO);
         pizzaObj.setId(id);
@@ -51,6 +87,13 @@ public class PizzaController {
     }
 
     @DeleteMapping(value = "/{id}")
+    @Operation(
+            summary = "Delete a Pizza by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ok"),
+                    @ApiResponse(responseCode = "404", description = "Object not Found")
+            }
+    )
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         pizzaService.delete(id);
         return ResponseEntity.noContent().build();
