7c07ff1a7fc7d3cec12e97ad8d27ed3960bb6314
João de Deus <jdjunior2@stefanini.com>
2022-05-11 14:31:54 -0300

    remove unused imports

--- src/test/java/com/joaodcpjunior/pizzas/controllers/PizzaControllerTest.java
+++ src/test/java/com/joaodcpjunior/pizzas/controllers/PizzaControllerTest.java
@@ -1,23 +1,18 @@
 package com.joaodcpjunior.pizzas.controllers;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
-
-import com.joaodcpjunior.pizzas.dtos.PizzaDTO;
 import com.joaodcpjunior.pizzas.entities.Pizza;
 import com.joaodcpjunior.pizzas.services.PizzaService;
-import com.joaodcpjunior.pizzas.services.exceptions.ObjectNotFoundException;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
 
 @WebMvcTest
 public class PizzaControllerTest {
